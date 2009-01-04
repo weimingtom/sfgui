8058a096223e65f30d8ae5a4d49f0bb5d3b9bb0a
TANGUY Arnaud <arn.tanguy@gmail.com>
2009-01-04 19:51:12 +0100

    Update docs. Beginig of class Checkbox

---
+++ checkbox.hpp
@@ -0,0 +1,52 @@
+/****************************
+* Auteur :Tanguy Arnaud
+* Date : 
+* Version : 1.0
+*
+* Copyright (C) 2008 TANGUY Arnaud
+* 
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ ******************************/
+
+/*!
+ * \file checkbox.hpp
+ * \brief Class which provides checkbox buttons 
+ * \author TANGUY Arnaud <arn.tanguy@gmail.com>
+ * \date 2009 
+ * \version 0.1
+ */
+
+
+#ifndef __CHECKBOX_HPP__
+#define __CHECKBOX_HPP__
+
+#include "object.hpp"
+
+/**
+ * \brief Class which provides checkbox buttons
+ *
+ * A checkbox is a special sort of button. When you click on it, the state is changed.
+ * It can be usefull for asking multiple-choice question.
+ */
+class Checkbox : public sfgui::Object
+{
+	public:
+		Checkbox(sf::RenderWindow *parentWindow);
+		Checkbox(sf::RenderWindow *parentWindow, std::string text, bool state=false);
+};
+
+
+#endif
+
